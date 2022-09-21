55a735d2449da2e6531e5d6157e475e5cbbf08c7
nikolozi14 <nikolozchichikashvili59@gmail.com>
2022-09-21 07:44:02 -0700

    first commit

---
+++ day1/lesson1.py
@@ -0,0 +1,7 @@
+name = "nika"
+surname = "chichikashvili"
+age = 14
+height = 165 
+knows_programming = False
+plays_valorant= True
+print(name + " " + surname + " " + str(age) +" "+ str(height) +" "+ str(knows_programming) + " "+ str(plays_valorant))
